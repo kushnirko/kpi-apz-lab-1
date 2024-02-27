29d3006f82623d4fbe0939e0346aeaaf6fb76b80
Nazg0r <gorchinshiy83@gmail.com>
2024-02-27 17:55:11 +0200

    Do some refactoring on app.go

--- app.go
+++ app.go
@@ -8,31 +8,54 @@ import (
 	"time"
 )
 
-const Port int = 8795
+const (
+	ip   string = "127.0.0.1"
+	port int    = 8795
+)
 
-type RequiredData struct {
-	Time string `json:"time"`
-}
+type Dictionary map[string]interface{}
 
-type message string
-
-func (m message) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
-	fmt.Fprint(resp, m)
-}
-func main() {
-
-	data := RequiredData{
-		Time: time.Now().Format(time.RFC3339),
-	}
-
-	jsonData, err := json.MarshalIndent(data, "", "  ")
+func (dict Dictionary) ToJSON() (string, error) {
+	jsonData, err := json.MarshalIndent(dict, "", "  ")
 	if err != nil {
-		fmt.Println("Serialization error:", err)
+		return "", fmt.Errorf("serialization error (%v)", err)
+	}
+	return string(jsonData), nil
+}
+
+func GetCurrentTime() string {
+	return time.Now().Format(time.RFC3339)
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprint(w, "Лабораторна робота 1")
+	if err != nil {
+		fmt.Println("Cannot write the response:", err)
 		return
 	}
+}
 
-	res := message(jsonData)
-	fmt.Println("Server is listening on localhost:" + strconv.Itoa(Port))
-	http.HandleFunc("/time", res.ServeHTTP)
-	http.ListenAndServe("localhost:"+strconv.Itoa(Port), nil)
+func Time(w http.ResponseWriter, r *http.Request) {
+	jsonTime, err := Dictionary{"time": GetCurrentTime()}.ToJSON()
+	if err != nil {
+		fmt.Println("Cannot convert the time to JSON format:", err)
+		return
+	}
+	_, err = fmt.Fprint(w, jsonTime)
+	if err != nil {
+		fmt.Println("Cannot write the response:", err)
+		return
+	}
+}
+
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/time", Time)
+
+	address := ip + ":" + strconv.Itoa(port)
+	fmt.Println("Server is listening on", address)
+	err := http.ListenAndServe(address, nil)
+	if err != nil {
+		fmt.Println("Cannot start the server:", err)
+	}
 }
